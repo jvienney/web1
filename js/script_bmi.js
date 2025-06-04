423d8a09ef3b2bfff5bf2d978d51f62415846e1b
Jean-Baptiste Vienney <jvienney@Jean-Baptistes-MacBook-Air.local>
2025-06-04 12:29:16 -0400

    Update

--- js/script_bmi.js
+++ js/script_bmi.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function computeBMI(weight,size){
-	return (weight/size**2)
+  	return (weight/((size/100)**2));
 }
+
+let user = {};
+user.weight = 70;
+user.size = 176;
+
+// alert("Hello world!");
+alert("Your BMI is " + (Math.trunc(100*computeBMI(user.weight,user.size)))/100 + ".");
+
